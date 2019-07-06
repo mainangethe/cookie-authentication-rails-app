d7450709d22e0b17e8ec774e0ae759de78bfba26
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-07-06 22:07:21 +0300

    Configure session to work in all environments
    - On api initial configuration it was only setup to consider a production environment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
-Rails.application.config.session_store :cookie_store,
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store,
   key: "_authentication_app",
   domain: "mradi-cookie-authentication.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store,
+  key: "_authentication_app"
+end

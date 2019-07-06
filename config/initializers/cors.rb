d340f58741c4eb6ee4376abb981dd84c33e2846e
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-07-06 20:41:29 +0300

    Add app skeleton & api config
    - Adding CORS & Session Store configuration
    - Loading from normal Rails app in order to utilize it's session management

---
+++ config/initializers/cors.rb
@@ -0,0 +1,18 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "http://localhost:3000"
+    resource "*", 
+      headers: :any, 
+      methods: [:get, :post, :put, :delete, :patch, :options, :head],
+      credentials: true
+  end
+  
+  allow do
+    origins "https://mradi-cookie-authentication.herokuapp.com"
+    resource "*", 
+      headers: :any, 
+      methods: [:get, :post, :put, :delete, :patch, :options, :head],
+      credentials: true
+  end
+  
+end
